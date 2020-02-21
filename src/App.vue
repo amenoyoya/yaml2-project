2cc29b02835cdee08baea41d7dffeaf838f6a200
amenoyoya <mtyswe3@gmail.com>
2020-02-22 02:47:52 +0900

    🍱 add: LiteGraph.js

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,26 @@
 <template>
   <section class="section">
-    <!--<nested-tree />-->
-    <diagram :model="model"></diagram>
+    <!-- <nested-tree /> -->
+    <!-- <diagram /> -->
+    <LiteGraph />
   </section>
 </template>
 
 <script>
-import { Diagram } from 'vue-diagrams'
+import LiteGraph from './components/LiteGraph';
 
 export default {
-  data() {
-    const diagramModel = new Diagram.Model()
-
-    const node1 = diagramModel.addNode("test2", 300, 200)
-    const inPort = node1.addInPort("test")
-
-    const node2 = diagramModel.addNode("test", 10, 300, 144, 80)
-    const node2OutPort = node2.addOutPort("testOut")
-    node2.addOutPort("testOut2")
-    node2.color = "#00cc66"
-
-    const node3 = diagramModel.addNode("test3", 10, 100, 72, 100)
-    const node3OutPort = node3.addOutPort("testOut3")
-    node3.color = "#cc6600"
-
-    diagramModel.addLink(node2OutPort, inPort)
-    diagramModel.addLink(node3OutPort, inPort)
-
-    return {
-      model: diagramModel
-    }
-  },
-
   components: {
-    Diagram
-  },
+    LiteGraph
+  }
 };
 </script>
+
+<style>
+html,
+body {
+  height: 100%;
+  position: relative;
+  margin: 0;
+}
+</style>
