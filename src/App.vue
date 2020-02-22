dbc85c48839f930d82b2420819b8476b6f9f8cb5
amenoyoya <mtyswe3@gmail.com>
2020-02-22 15:17:58 +0900

    🍱 add: Rete.js

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,48 @@
 <template>
   <section class="section">
-    <h2 class="title">BaklavaJS</h2>
-    <baklava-editor :plugin="viewPlugin"></baklava-editor>
+    <div id="rete"></div>
   </section>
 </template>
 
 <script>
-import { Editor } from '@baklavajs/core';
-import { ViewPlugin } from '@baklavajs/plugin-renderer-vue';
+import Rete from 'rete';
+import ConnectionPlugin from 'rete-connection-plugin';
+import VueRenderPlugin from 'rete-vue-render-plugin';
+
+// 数値ノード
+class NumComponent extends Rete.Component {
+  constructor() {
+    super('Number');
+  }
+
+  builder(node) {
+    const numSocket = new Rete.Socket('Number value');
+    const out = new Rete.Output('num', 'Number', numSocket);
+
+    node.addOutput(out);
+  }
+
+  worker(node, inputs, outputs) {
+    outputs['num'] = node.data.num;
+  }
+}
 
 export default {
-  data() {
-    return {
-      editor: new Editor(),
-      viewPlugin: new ViewPlugin()
-    }
-  },
-  created() {
-    this.editor.use(this.viewPlugin);
+  mounted() {
+    const editor = new Rete.NodeEditor('test@rete', document.querySelector('#rete'));
+    editor.use(ConnectionPlugin);
+    editor.use(VueRenderPlugin);
+    
+    const numComponent = new NumComponent();
+    editor.register(numComponent);
+
+    const engine = new Rete.Engine('demo@0.1.0');
+    engine.register(numComponent);
+
+    editor.on('process nodecreated noderemoved connectioncreated connectionremoved', async () => {
+      await engine.abort();
+      await engine.process(editor.toJSON());
+    });
   }
 };
 </script>
